0851dd30be76226de44b3069884a1faed3810779
ydy939785177 <47445532+ydy939785177@users.noreply.github.com>
2019-03-25 22:49:34 +0800

    A同学上传并修改了代码
    添加了一个关闭资源的方法

--- src/main/java/helloGit.java
+++ src/main/java/helloGit.java
@@ -7,10 +7,14 @@ public class helloGit {
         System.out.println("开始使用");
         getConnection();
         System.out.println("使用完毕");
+        close();
     }
 
     public static void getConnection() {
         System.out.println("helloGit.getConnection");
         System.out.println("true = " + true);
+    }
+    public static void close(){
+     System.out.println("开始关闭资源");
     }
 }
