8e9e2c8a3792a8447510e4ebefd4013c14a04a1e
ydy939785177 <939785177@qq.com>
2019-03-25 22:47:14 +0800

    项目第一次改动

---
+++ src/main/java/helloGit.java
@@ -0,0 +1,16 @@
+/**
+ * @author ydy
+ * @create 2019-03-25 22:44
+ */
+public class helloGit {
+    public static void main(String[] args) {
+        System.out.println("开始使用");
+        getConnection();
+        System.out.println("使用完毕");
+    }
+
+    public static void getConnection() {
+        System.out.println("helloGit.getConnection");
+        System.out.println("true = " + true);
+    }
+}
